b21373a4acac2b1b9251f84f5dcaccbc1e4bceea
Guruprasad J <guruprasad.jg@gmail.com>
2012-11-18 20:53:46 +0000

    Minor cleanup

--- jimini/app/website/runBackgroundJob.py
+++ jimini/app/website/runBackgroundJob.py
@@ -30,28 +30,20 @@ def fetchBody(coupon):
 print "Running Mail Parser and Mail Sender script"
 
 orders = retrievemail.parseEmailAndFetchOrderInfo()
-#print orders
 for order in orders:
     print "Processing Order:" + str(order)
-    #print order['from']
     generatedMail = fetchGeneratedMail(order['to'])
-    print generatedMail 
     userMail = fetchUserMailForGeneratedMail(generatedMail)
     
     #print order['to']
     coupon= order.get('coupon',None)
-    print coupon
 
     #construct the email
     subject = 'Your Origami is ready to print!'
     body =  fetchBody(coupon)
-    print body
     fromMail = 'jimini.Origami@gmail.com'
 
     #send the email
     sendmail.send_jemini_email(fromMail, userMail, subject , body)
 
 print "Done processing all orders!"
-
-
-
