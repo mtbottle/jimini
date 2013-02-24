b1a74ce9eb8cfbfa62fc6b79d70ca899a28365fb
Thibaut Labarre <Asimov4@gmail.com>
2013-02-23 22:27:05 -0800

    Repaired media settings

--- jimini/urls.py
+++ jimini/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+import settings
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -9,7 +10,7 @@ urlpatterns = patterns('',
     url(r'^how_this_works$', 'jimini.app.website.views.how_this_works', name='how_this_works'),
     url(r'^wrap/(?P<coupon>.+)$', 'jimini.app.website.views.wrap_page', name='wrap'),
     url(r'^coupon/(?P<coupon>.+)$', 'jimini.app.website.views.coupon_redirect', name='redirect'),
-	url(r'^user_page', 'jimini.app.website.views.user_page', name='user_page'),
+    url(r'^user_page', 'jimini.app.website.views.user_page', name='user_page'),
 
     # Examples:
     # url(r'^$', 'jimini.views.home', name='home'),
@@ -20,4 +21,8 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.MEDIA_ROOT,
+        }),
 )
