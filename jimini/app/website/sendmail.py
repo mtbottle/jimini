dc49e5f4e87116da84979a1ec36e6396a30fe2c3
bfortuner <bfortuner@gmail.com>
2013-10-29 20:35:32 -0700

    updated sendmail function to incorporate jinja templates and drafted initial confirmation email

--- jimini/app/website/sendmail.py
+++ jimini/app/website/sendmail.py
@@ -1,10 +1,24 @@
 from email.mime.text import MIMEText
 from subprocess import Popen, PIPE
+from jinja2 import Environment, FileSystemLoader
+import os
+app_dir = os.path.dirname(__file__) # get current directory 
 
-def send_jemini_email(email_from, recipient, subject, content):
-	msg = MIMEText(content)
+template_dir = os.path.join(app_dir, '../../../templates/email_templates')
+loader = FileSystemLoader(template_dir)
+env = Environment(loader=loader)
+
+
+template = env.get_template('order_confirm.html')
+html = template.render()
+email_from = 'confirmation@jimini.co'
+email_to = 'bfortuner@gmail.com'
+subject = "Your Jimini Order!"
+
+def send_jimini_email(email_from, email_to, subject, html):
+	msg = MIMEText(html, 'html')
 	msg["From"] = email_from
-	msg["To"] = recipient
+	msg["To"] = email_to
 	msg["Subject"] = subject
 
 	# open process to start sending email via sendmail
@@ -12,7 +26,4 @@ def send_jemini_email(email_from, recipient, subject, content):
 	p.communicate(msg.as_string())
 
 if __name__ == '__main__':
-	send_jemini_email('jimini.origami@gmail.com', \
-		'mtsyne@gmail.com', \
-		'Your Jimini Order', \
-		'This is the content')
+	send_jimini_email(email_from, email_to, subject, html)
